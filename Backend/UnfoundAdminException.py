60c9e5f29c67d61e79d4c343d7feaa4c2d025584
VitomirDragan <vitomir_dragan86@yahoo.com>
2020-03-10 15:55:07 +0200

    Exceptions added

---
+++ Backend/UnfoundAdminException.py
@@ -0,0 +1,6 @@
+class UnfoundAdminException(Exception):
+    def __init__(self, message="This admin is not present in the database"):
+        self.__message = message
+
+    def __str__(self):
+        return self.message
